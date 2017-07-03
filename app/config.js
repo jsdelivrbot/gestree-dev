2658f412bb51853b46cdbcb900e16454a67daa65
JoKord <jtpcordeiro@gmail.com>
2017-07-03 14:46:24 +0100

    production

--- app/config.js
+++ app/config.js
@@ -3,7 +3,7 @@
 
     angular.module('unicerApp.configs', [])
         .constant('Globals', {
-            'ENVIRONMENT': 'Production',
+            'ENVIRONMENT': 'production',
             'URL_WMS': {
                 'development': "http://localhost:3002/geoserver/wms",
                 'production': "/geoserver/wms"
