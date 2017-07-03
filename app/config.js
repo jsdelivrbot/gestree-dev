5423c04ba34fe05101783e351cb06f590e456cbf
JoKord <jtpcordeiro@gmail.com>
2017-07-03 14:40:24 +0100

    changed

--- app/config.js
+++ app/config.js
@@ -6,15 +6,15 @@
             'ENVIRONMENT': 'Development',
             'URL_WMS': {
                 'Development': "http://localhost:3002/geoserver/wms",
-                'Production': "http://gistree.espigueiro.pt:3002/geoserver/wms"
+                'Production': "/geoserver/wms"
             },
             'URL_WFS': {
                 'Development': "http://localhost:3002/geoserver/wfs",
-                'Production': "http://gistree.espigueiro.pt:3002/geoserver/wfs"
+                'Production': "/geoserver/wfs"
             },
             'URL_PRINT': {
                 'Development': "http://localhost:3002/geoserver/pdf",
-                'Production': "http://gistree.espigueiro.pt:3002/geoserver/pdf"
+                'Production': "/geoserver/pdf"
             }
         });
 })();
