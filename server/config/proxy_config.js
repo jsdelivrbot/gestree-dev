1ce7266a969a811f8c116552e5bb66383a1f5b01
JoKord <jtpcordeiro@gmail.com>
2017-07-03 11:42:55 +0100

    changed proxy config

--- server/config/proxy_config.js
+++ server/config/proxy_config.js
@@ -1,9 +1,9 @@
 module.exports = {
     development: {
-        url_wms: 'http://gistree.espigueiro.pt:8081/geoserver/wms/',
-        url_wfs: 'http://gistree.espigueiro.pt:8081/geoserver/wfs/',
-        url_print: 'http://gistree.espigueiro.pt:8081/geoserver/pdf/',
-        url_print_create: 'http://gistree.espigueiro.pt:8081/geoserver/pdf/create.json'
+        url_wms: 'http://localhost:8081/geoserver/wms/',
+        url_wfs: 'http://localhost:8081/geoserver/wfs/',
+        url_print: 'http://localhost:8081/geoserver/pdf/',
+        url_print_create: 'http://localhost:8081/geoserver/pdf/create.json'
     },
     production: {
         url_wms: process.env.WMS_URL,
