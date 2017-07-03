518d698c87e27e773214a5f0c7240c7466070337
JoKord <jtpcordeiro@gmail.com>
2017-07-03 14:28:12 +0100

    back to true

--- app/js/Modules/Main/MainModule.js
+++ app/js/Modules/Main/MainModule.js
@@ -56,7 +56,7 @@
                 }).otherwise({
                     redirectTo: '/'
                 });
-            /*$locationProvider.html5Mode(false);*/
+            $locationProvider.html5Mode(false);
         }])
         .run(['Map', function (Map) {
             Map.setTarget("map");
