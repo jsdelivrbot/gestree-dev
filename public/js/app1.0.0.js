f9fbee70009edea25cabcf061ef11c737bd8fd1c
JoKord <jtpcordeiro@gmail.com>
2017-11-28 18:28:38 +0000

    clear

--- public/js/app1.0.0.js
+++ public/js/app1.0.0.js
@@ -7,12 +7,8 @@ angular
     'ngRoute'
   ])
   .constant('GlobalURLs', {
-    // DEV
     host: "http://gistree.espigueiro.pt:8081",
     print: "http://gistree.espigueiro.pt:8081/print-servlet-3.8.0/print/gestree/report.pdf"
-    // PROD
-    //host: "http://localhost:8081",
-    //print: "http://localhost:8081/print-servlet-3.8.0/print/gestree/report.pdf"
   });
 angular
   .module('unicerApp')
@@ -1495,6 +1491,96 @@ function TreesHttp($q, $http) {
 }
 angular
   .module('unicerApp')
+  .service('SortingService', SortingService);
+
+SortingService.$inject = ['DefaultInterventionData'];
+
+function SortingService(Defaults){
+
+  return {
+    orderBySeasonYear: orderBySeasonYear
+  };
+
+  function orderBySeasonYear(intervention) {
+    var date = new Date(
+      intervention.year,
+      Defaults.getSeasons().indexOf(intervention.season) + 3,
+      1
+    );
+    return date.getTime();
+  }
+}  
+angular
+  .module('unicerApp')
+  .service('WFSStyles', WFSStyles);
+
+function WFSStyles() {
+
+  var _styles = {
+    defaultStyle: new ol.style.Style({
+      image: new ol.style.Circle({
+        radius: 3,
+        fill: new ol.style.Fill({
+          color: [24, 72, 26, 0.8]
+        }),
+        stroke: new ol.style.Stroke({
+          color: [0, 0, 0, 1]
+        }),
+      })
+    }),
+    selected: new ol.style.Style({
+      image: new ol.style.Circle({
+        radius: 4,
+        fill: new ol.style.Fill({
+          color: [72, 24, 70, 1]
+        }),
+        stroke: new ol.style.Stroke({
+          color: [0, 0, 0, 1],
+          width: 2
+        }),
+      }),
+      zIndex: 100
+    }),
+    intervention: new ol.style.Style({
+      image: new ol.style.Circle({
+        radius: 4,
+        fill: new ol.style.Fill({
+          color: [72, 15, 15, 1]
+        }),
+        stroke: new ol.style.Stroke({
+          color: [0, 0, 0, 1],
+          width: 2
+        })
+      }),
+      zIndex: 50
+    })
+  };
+
+  return {
+    treeSelected: treeSelected,
+    treeDefault: treeDefault,
+    treeIntervention: treeIntervention,
+    treeHighlight: treeHighlight
+  }
+
+  function treeSelected(){
+    return _styles.selected;
+  }
+  function treeDefault() {
+    return _styles.defaultStyle;
+  }
+  function treeIntervention(feature, layer) {
+    return feature.getProperties().has_inter ? _styles.intervention : _styles.defaultStyle;
+  }
+  function treeHighlight(selectedFeatureID) {
+    return function (feature, layer) {
+      return selectedFeatureID === feature.getId() ? _styles.selected : _styles.defaultStyle;
+    }
+  }
+
+}
+angular
+  .module('unicerApp')
   .service('DefaultInterventionData', DefaultInterventionData);
 
 DefaultInterventionData.$inject = ['$q', 'InterventionTypesHttp'];
@@ -2299,93 +2385,3 @@ function TreeDetailsService($q, TreesHttp, $rootScope, Dirty) {
   }
 
 }  
-angular
-  .module('unicerApp')
-  .service('SortingService', SortingService);
-
-SortingService.$inject = ['DefaultInterventionData'];
-
-function SortingService(Defaults){
-
-  return {
-    orderBySeasonYear: orderBySeasonYear
-  };
-
-  function orderBySeasonYear(intervention) {
-    var date = new Date(
-      intervention.year,
-      Defaults.getSeasons().indexOf(intervention.season) + 3,
-      1
-    );
-    return date.getTime();
-  }
-}  
-angular
-  .module('unicerApp')
-  .service('WFSStyles', WFSStyles);
-
-function WFSStyles() {
-
-  var _styles = {
-    defaultStyle: new ol.style.Style({
-      image: new ol.style.Circle({
-        radius: 3,
-        fill: new ol.style.Fill({
-          color: [24, 72, 26, 0.8]
-        }),
-        stroke: new ol.style.Stroke({
-          color: [0, 0, 0, 1]
-        }),
-      })
-    }),
-    selected: new ol.style.Style({
-      image: new ol.style.Circle({
-        radius: 4,
-        fill: new ol.style.Fill({
-          color: [72, 24, 70, 1]
-        }),
-        stroke: new ol.style.Stroke({
-          color: [0, 0, 0, 1],
-          width: 2
-        }),
-      }),
-      zIndex: 100
-    }),
-    intervention: new ol.style.Style({
-      image: new ol.style.Circle({
-        radius: 4,
-        fill: new ol.style.Fill({
-          color: [72, 15, 15, 1]
-        }),
-        stroke: new ol.style.Stroke({
-          color: [0, 0, 0, 1],
-          width: 2
-        })
-      }),
-      zIndex: 50
-    })
-  };
-
-  return {
-    treeSelected: treeSelected,
-    treeDefault: treeDefault,
-    treeIntervention: treeIntervention,
-    treeHighlight: treeHighlight
-  }
-
-  function treeSelected(){
-    return _styles.selected;
-  }
-  function treeDefault() {
-    return _styles.defaultStyle;
-  }
-  function treeIntervention(feature, layer) {
-    return feature.getProperties().has_inter ? _styles.intervention : _styles.defaultStyle;
-  }
-  function treeHighlight(selectedFeatureID) {
-    return function (feature, layer) {
-      return selectedFeatureID === feature.getId() ? _styles.selected : _styles.defaultStyle;
-    }
-  }
-
-}
