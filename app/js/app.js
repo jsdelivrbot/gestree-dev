5ee73439c2c40289d0ef5338b936cca1e4d309c3
JoKord <jtpcordeiro@gmail.com>
2017-11-28 17:37:33 +0000

    Final one

--- app/js/app.js
+++ app/js/app.js
@@ -8,9 +8,9 @@ angular
   ])
   .constant('GlobalURLs', {
     // DEV
-    host: "http://gistree.espigueiro.pt:8081",
-    print: "http://gistree.espigueiro.pt:8081/print-servlet-3.8.0/print/gestree/report.pdf"
+    //host: "http://gistree.espigueiro.pt:8081",
+    //print: "http://gistree.espigueiro.pt:8081/print-servlet-3.8.0/print/gestree/report.pdf"
     // PROD
-    //host: "localhost:8081",
-    //print: "localhost:8081/print-servlet-3.8.0/print/gestree/report.pdf"
+    host: "localhost:8081",
+    print: "localhost:8081/print-servlet-3.8.0/print/gestree/report.pdf"
   });
